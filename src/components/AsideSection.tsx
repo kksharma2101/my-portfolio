ea86edd840f5cade34ad17d69eeaa5fc718c102a
kamal sharma <kamalbsr2101@gmail.com>
2024-11-05 10:28:14 +0530

    change title name and logo

--- src/components/AsideSection.tsx
+++ src/components/AsideSection.tsx
@@ -1,65 +1,76 @@
 import React from "react";
 import Link from "next/link";
 import {
-    RiExternalLinkLine,
-    RiGithubFill,
-    RiInstagramFill,
-    RiLinkedinBoxFill,
-    RiTwitterFill,
+  RiExternalLinkLine,
+  RiGithubFill,
+  RiInstagramFill,
+  RiLinkedinBoxFill,
+  RiTwitterFill,
 } from "react-icons/ri";
 
 interface TableOfContentItem {
-    id: string;
-    title: string;
+  id: string;
+  title: string;
 }
 
 interface AsideSectionProps {
-    tableOfContent: TableOfContentItem[];
+  tableOfContent: TableOfContentItem[];
 }
 
 const AsideSection: React.FC<AsideSectionProps> = ({ tableOfContent }) => {
-    return (
-        <aside className="w-[30%] border border-black sticky top-16 rounded-md bg-black p-4 hidden md:block">
+  return (
+    <aside className="w-[30%] border-none sticky top-16 rounded-md bg-black p-4 hidden md:block">
+      <div className="">
+        <h2 className="font-normal m-1 text-white text-2xl ">
+          Table of Content
+        </h2>
 
-            <div className="">
+        <ul className="my-3 text-white list-inside list-disc text-sm">
+          {tableOfContent.map((item, index) => (
+            <li key={index} className="mb-2">
+              <a href={`#${item.id}`}>{item.title}</a>
+            </li>
+          ))}
+          <li>
+            <Link href="">Download My Resume</Link>
+          </li>
+        </ul>
 
-                <h2 className="font-normal m-1 text-white text-2xl ">Table of Content</h2>
+        <p className="text-white my-5 pl-1">
+          <Link
+            href="mailto:kamalbsr2101@gmail.com"
+            className="flex items-center gap-2"
+          >
+            kamalbsr2101@gmail.com
+            <RiExternalLinkLine className="text-white" />
+          </Link>
+        </p>
+      </div>
 
-                <ul className="my-3 text-white list-inside list-disc text-sm">
-                    {tableOfContent.map((item, index) => (
-                        <li key={index} className="mb-2">
-                            <a href={`#${item.id}`}>{item.title}</a>
-                        </li>
-                    ))}
-                    <li><Link href="">Download My Resume</Link></li>
-                </ul>
-
-                <p className="text-white my-5 pl-1">
-                    <Link href="mailto:kamalbsr2101@gmail.com" className="flex items-center gap-2">
-                        kamalbsr2101@gmail.com
-                        <RiExternalLinkLine className="text-white" />
-                    </Link>
-                </p>
-
-            </div>
-
-            <div className="flex items-center justify-center gap-4">
-                <Link href="https://github.com/kksharma2101" target="_blank" className="text-white text-2xl ml-2">
-                    <RiGithubFill />
-                </Link>
-                <Link href="https://www.linkedin.com/in/kksharma21/" target="_blank" className="text-white text-2xl ml-2">
-                    <RiLinkedinBoxFill />
-                </Link>
-                <Link href="/" target="_blank" className="text-white text-2xl ml-2">
-                    <RiTwitterFill />
-                </Link>
-                <Link href="/" target="_blank" className="text-white text-2xl ml-2">
-                    <RiInstagramFill />
-                </Link>
-            </div>
-
-        </aside>
-    );
+      <div className="flex items-center justify-center gap-4">
+        <Link
+          href="https://github.com/kksharma2101"
+          target="_blank"
+          className="text-white text-2xl ml-2"
+        >
+          <RiGithubFill />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/kksharma21/"
+          target="_blank"
+          className="text-white text-2xl ml-2"
+        >
+          <RiLinkedinBoxFill />
+        </Link>
+        <Link href="/" target="_blank" className="text-white text-2xl ml-2">
+          <RiTwitterFill />
+        </Link>
+        <Link href="/" target="_blank" className="text-white text-2xl ml-2">
+          <RiInstagramFill />
+        </Link>
+      </div>
+    </aside>
+  );
 };
 
 export default AsideSection;
