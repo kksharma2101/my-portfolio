4548988189ea6afc1e32f14b7359e61bebea41d3
kamal-kumar-ast <kamal@astconsulting.in>
2025-01-02 22:01:13 +0530

    add updated resume

--- src/components/home/Introduction.tsx
+++ src/components/home/Introduction.tsx
@@ -33,7 +33,7 @@ const Introduction = () => {
               Certificate <RiExternalLinkLine />
             </a>
             <a
-              href="latest-resume.pdf"
+              href="new-resume.pdf"
               className="flex items-center px-3 py-2 bg-white font-medium text-black rounded-md gap-1"
             >
               Resume <BsDownload />
