6e487b20fb346caf0f9a0f3c155e55beaf9c0def
kamal-kumar-ast <kamal@astconsulting.in>
2025-04-04 12:53:11 +0530

    add new resume

--- src/components/home/Introduction.tsx
+++ src/components/home/Introduction.tsx
@@ -33,7 +33,7 @@ const Introduction = () => {
               Certificate <RiExternalLinkLine />
             </a>
             <a
-              href="myResume.pdf"
+              href="MERN-resume.pdf"
               className="flex items-center px-3 py-2 bg-white font-medium text-black rounded-md gap-1"
             >
               Resume <BsDownload />
